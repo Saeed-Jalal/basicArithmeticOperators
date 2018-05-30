4829b2ad129c5adca875b4c581ea077c50b205d8
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-05-30 16:46:18 +0300

    How arithmetic operators work in Java

---
+++ src/basicArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class basicArithmeticOperators {
+    /* Basic arithmetic operators are : +, -, /, %
+    % is for Modulo and it returns remainder.
+     */
+    public static void main(String args[]){
+        int num1 = 510;
+        int num2 = 310;
+
+
+        System.out.println("num1 + num2:" +(num1+num2));
+        System.out.println("num1-num2:" +(num1-num2));
+        System.out.println("num1/num2:" +(num1/num2));
+        System.out.println("num1%num2:" +(num1%num2));
+    }
+}
